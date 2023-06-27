097531e81102c30b864fe5b40330019d25178ac1
Gavin <gavinjsho@gmail.com>
2023-06-26 19:28:32 -0700

    Populate hall of fame

--- src/components/HallOfFame.jsx
+++ src/components/HallOfFame.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/react-splide/css';
 
@@ -6,31 +7,47 @@ import Profile from "./Profile";
 
 export default function HallOfFame() {
 
-    return (
-        <div id="halloffame" className=" w-full h-[500px] bg-[#f5f5f5] flex flex-col justify-center items-center">
-            <div data-aos="fade-up">
-                <h1 className="text-5xl text-black font-bold text-center">Hall Of Fame</h1>
-                <div className="m-10">
-                    <Splide options={{
-                        type: 'loop',
-                        perPage: 1,
-                        arrows: false,
-                        pagination: false,
-                        width: 500,
-                        autoplay: true,
-                        gap: '1rem',
-                    }}>
-                        <SplideSlide>
-                            <Profile />
-                        </SplideSlide>
-                        <SplideSlide>
-                            <Profile />
-                        </SplideSlide>
-                        <SplideSlide>
-                            <Profile />
-                        </SplideSlide>
+    const [data, setData] = useState();
+    const [isLoading, setIsLoading] = useState(true);
 
-                    </Splide>
+    useEffect(() => {
+        axios.get('https://wordle-api.herokuapp.com/api/hall-of-fame')
+            .then(response => {
+                setData(response.data);
+                setIsLoading(false); // Set loading state to false
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                setIsLoading(false); // Set loading state to false even on error
+            });
+    }, []);
+
+    return (
+        <div id="halloffame" className=" w-full h-[500px] bg-[#f5f5f5] text-black flex justify-center items-center">
+            <div className="border-2 border-green-600 rounded-lg">
+                <div data-aos="fade-up">
+                    <h1 className="text-5xl text-black font-semibold text-center m-12">Hall Of Fame 🏆</h1>
+                    <div className="my-12">
+                        <Splide options={{
+                            type: 'loop',
+                            perPage: 1,
+                            arrows: false,
+                            pagination: false,
+                            width: 500,
+                            autoplay: true,
+                            gap: '1rem',
+                        }}>
+                            {isLoading ? null : (
+                                data.map((value) => (
+                                    Object.entries(value.Winners).map(([name, score]) => (
+                                        <SplideSlide key={name}>
+                                            <Profile month={value.Month} year={value.Year} name={name} score={score} />
+                                        </SplideSlide>
+                                    ))
+                                ))
+                            )}
+                        </Splide>
+                    </div>
                 </div>
             </div>
         </div>
