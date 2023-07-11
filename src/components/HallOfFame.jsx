f34eeb2175f84c7be5a6d093ccdddf07e08691e1
Gavin <gavinjsho@gmail.com>
2023-07-10 19:15:53 -0700

    Styling hall-of-fame

--- src/components/HallOfFame.jsx
+++ src/components/HallOfFame.jsx
@@ -29,7 +29,7 @@ export default function HallOfFame() {
         <div id="halloffame" style={{ backgroundImage: "url('https://www.transparenttextures.com/patterns/flowers.png')" }} className="w-full bg-[#e6f1e8]">
             <div className="text-black flex justify-center items-center py-12" data-aos="fade-up">
                 <div className="w-[100%] h-full">
-                    <h1 className="lg:text-[3rem] text-[2rem] text-black font-bold text-center my-4">🏆 Hall Of Fame 🏆</h1>
+                    <h1 className="lg:text-[3rem] text-[2rem] text-green-900 font-bold text-center my-4">🏆 Hall Of Fame 🏆</h1>
                     <div className="grid xl:grid-cols-3 lg:grid-cols-2 grid-cols-1">
 
                         {isLoading ? <div className="mb-24 text-xl">Loading slides...</div> : (
